1d86902054a6c720ad1683b689b052afa438010d
lenizelira <lenizelira@gmail.com>
2020-11-06 21:56:12 -0300

    exercicio semana 13 - api games

---
+++ src/models/repository.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+//mongo://nomehost:porta/nomedatabase
+const DB_URL = "mongodb://localhost:27017/reprograma"
+
+const connect = () =>{
+    mongoose.connect(DB_URL, {useNewUrlParser: true})
+    const connection = mongoose.connection
+    connection.on("error", () => console.error("Erro ao conectar com o MongoDB"))
+    connection.once("open", () => console.log("Estamos conectadas!"))
+}
+
+module.exports = {
+    connect
+}
